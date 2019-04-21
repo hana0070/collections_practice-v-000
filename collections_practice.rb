cad4799fdd3d96123f5fb70b7a41b3d2290aa762
HANA LEANDER <hana0070@gmail.com>
2019-04-21 14:46:06 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -1,1 +1,58 @@
+require 'pry'
 
+def sort_array_asc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+end
+
+def sort_array_desc(array)
+  array.sort do |a, b|
+    a <=> b
+  end
+  array.sort.reverse
+end
+
+def sort_array_char_count(array)
+  array.sort {|a, b| a.length <=> b.length}
+end
+
+#def swap_elements_from_to(array, index, destination_index)
+#  array.each do |index, destination_index|
+#    index = destination_index
+#    destination_index = index
+#  end
+#end
+
+def swap_elements(array)
+
+  array[1], array[2] = array[2], array[1]
+  array
+  #swap_elements_from_to(array, 1, 2)
+end
+
+
+def reverse_array(array)
+  array.reverse
+end
+
+def kesha_maker(array)
+  array.each do |index|
+    index.split
+  end
+  array
+end
+
+def find_a(array)
+  array.find
+
+end
+
+def sum_array(array)
+    array.sum
+end
+
+def add_s(array)
+  
+
+end
