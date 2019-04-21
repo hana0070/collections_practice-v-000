1a29bf465e77b57a1b131e441dd95ece26f57fe8
HANA LEANDER <hana0070@gmail.com>
2019-04-21 22:04:56 +0000

    Automatically backed up by Learn

--- collections_practice.rb
+++ collections_practice.rb
@@ -44,6 +44,9 @@ def kesha_maker(array)
   end
   array
   #"Super bag".sub "g", "d" # => "Super bad"
+  # To replace a character at a specified position :
+  #str[0..pos-1] + s + str[pos+1..-1]      # new String
+  #str[pos,1] = s                          # modify their receiver  
 end
 
 def find_a(array)
